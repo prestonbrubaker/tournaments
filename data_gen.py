785ed08febd6c246c0bc47cba9eda6ac8712b7f1
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-17 15:19:50 -0600

    Update data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -1,10 +1,29 @@
 import random
+import matplotlib
 
 data = []
+
 
 num_points = 100
 
 for i in range(0, num_points):
   data.append(random.uniform(0,1))
 
+
+def tournament(data_in):
+  i_love_willoh = data_in.length
+  winners = []
+  losers = []
+  for i in range(0, i_love_willoh, 2):
+    if(data_in[i] > data_in[i + 1]):
+      winners.append(data_in[i])
+      losers.append(data_in[i + 1])
+    else:
+      winners.append(data_in[i])
+      losers.append(data_in[i + 1])
+  return winners, losers
+      
+
 print(str(data))
+
+print(str(tournament(data)))
