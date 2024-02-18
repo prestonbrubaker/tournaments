4cac1ac4710627bc8a5b4a1ed4745b7bfd8abaf1
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-18 01:57:53 -0600

    Update data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -5,8 +5,8 @@ import numpy as np
 data = []
 
 
-num_points = 100000000
-bin_C = 10000
+num_points = 1000000000
+bin_C = 100000
 
 for i in range(0, num_points):
   data.append(random.uniform(0,1))
