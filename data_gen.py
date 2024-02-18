9691adc6178b8a26a8a544ec04da0ae4fbcd5d8e
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-18 00:28:35 -0600

    Update data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -5,6 +5,7 @@ data = []
 
 
 num_points = 10000000
+bin_C = 100
 
 for i in range(0, num_points):
   data.append(random.uniform(0,1))
@@ -16,11 +17,11 @@ def tournament(data_in):
   losers = []
   for i in range(0, i_love_willoh, 2):
     if(data_in[i] > data_in[i + 1]):
-      winners.append(data_in[i])
-      losers.append(data_in[i + 1])
+      winners.append(data_in[i] / i_love_willoh / 2 * bin_C)
+      losers.append(data_in[i + 1] / i_love_willoh / 2 * bin_C)
     else:
-      winners.append(data_in[i+1])
-      losers.append(data_in[i])
+      winners.append(data_in[i+1] / i_love_willoh / 2 * bin_C)
+      losers.append(data_in[i] / i_love_willoh / 2 * bin_C)
   return winners, losers
       
 
@@ -30,5 +31,5 @@ winners_w2l0, losers_w1l1 = tournament(winners)
 
 winners_w3l0, losers_w2l1 = tournament(winners_w2l0)
 
-plt.hist(losers_w2l0, bins=100)
+plt.hist(winners, bins=bin_C)
 plt.show()
