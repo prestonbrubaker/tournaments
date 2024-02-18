45f2a4821cb1f8e0a434a5dc184f886e70bfd66f
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-18 00:35:26 -0600

    Update data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -31,5 +31,5 @@ winners_w2l0, losers_w1l1 = tournament(winners)
 
 winners_w3l0, losers_w2l1 = tournament(winners_w2l0)
 
-plt.hist(losers_w3l0, bins=bin_C, density=True)
+plt.hist(winners_w3l0, bins=bin_C, density=True)
 plt.show()
